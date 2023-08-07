d65002eb7b98c1fc79d6d27df0fe41e1762e8f6b
Hi-ak <hiroyuki_akiyama2007@icloud.com>
2023-08-08 06:37:02 +0900

    💄 stylesheets separated by components

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,14 @@
-import styles from "../styles/Home.module.css";
+import classes from "./Footer.module.scss";
 export function Footer() {
   return (
-    <footer>
+    <footer className={classes.footer}>
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
       >
         Presented performance by{' '}
-        <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        <img src="/vercel.svg" alt="Vercel" className={classes.logo} />
       </a>
     </footer>
   )
--- components/Headline.js
+++ components/Headline.js
@@ -1,14 +1,14 @@
 import React from "react";
-import styles from "../styles/Home.module.css";
+import classes from "./Headline.module.scss";
 
 export function Headline(props) {
   return ( //react component = <>
     <div> 
-      <h1 className={styles.title}>
-        {props.page} Page
+      <h1 className={classes.title}>
+        <a href={props.page == "index" ? "/" : "/" + props.page}>{props.page}</a> Page
       </h1>
 
-      <p className={styles.description}>
+      <p className={classes.description}>
         Get started by editing {" "}
         { props.children }
       </p>
