e5e54bcce010c868734619b8bb8a1c71626ca606
Hi-ak <hiroyuki_akiyama2007@icloud.com>
2023-08-08 10:37:54 +0900

    🥅 fixed freaking comment out error

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,16 +1,17 @@
 import 'src/styles/globals.css'
 import Head from 'next/head'
 
-function MyApp({ Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
+  
   return (
-    <>  {/* ここでは react fragmentを使う。（divを増やしたくないし、親のflexとかが崩れるとかのリスクがないから）*/}
+    <> 
+    {/* ここでは react fragmentを使う。（divを増やしたくないし、親のflexとかが崩れるとかのリスクがないから）*/}
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps}/>
     </>
-  )
-  
+    );
 }
 
-export default MyApp
+export default MyApp;
